4d7a93c3d95e357706961a4326ecf5ac79da0c8e
d-khalaf02 <d.khalaf@xsigns.de>
2024-10-07 14:02:44 +0200

    modifing index.ts,package.json

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import 'reflect-metadata'
 import { IBooking, IPerson, IProperty, Person, PERSON_TYPES, TYPES } from '@fewo-monorepo/entities'
 import { emptyBooking, mockBooking, mockPerson, mockProperty, unknownProperty, container } from '@fewo-monorepo/utils'
 
-let property : IProperty
-let booking: IBooking
+let property : IProperty = unknownProperty
+let booking: IBooking = emptyBooking
 
 try{
     const owner = mockPerson(PERSON_TYPES.Owner)
+
     const propertyManager = mockPerson(PERSON_TYPES.PropertyManager)
 
     const guest1 = mockPerson(PERSON_TYPES.Guest)
@@ -23,16 +24,16 @@ try{
         guest2,
         guest3
     ])
+
     container.bind<IProperty>(TYPES.Property).toConstantValue(property)
 
     booking = mockBooking()
+    console.log(':::::::::: HERE 1')
 } catch(error){
     console.log(error)
-
-    property = unknownProperty
-    booking = emptyBooking
 }
 
+console.log(property)
 console.log(booking)
 
 export { property }
